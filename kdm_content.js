5aa5c065883c1f5fdb74d0b4584d60d9d8158a8b
Sean Dobberstein <sdobbers@gmail.com>
2017-01-04 21:20:53 -0500

    Adding Core Game as an Add-On

--- kdm_content.js
+++ kdm_content.js
@@ -264,7 +264,7 @@
 
   const ITEMS = [
     // Main Component
-    { title: "1.5 Core Game", price: 250, contentType: CONTENT_TYPES.MAIN_COMPONENT, addon: false, wave: 1 },
+    { title: "1.5 Core Game", price: 300, contentType: CONTENT_TYPES.MAIN_COMPONENT, addon: true, wave: 1 },
     { title: "1.5 Update Pack", price: 60, contentType: CONTENT_TYPES.MAIN_COMPONENT, addon: false, wave: 1 },
 
     // New Expansions
